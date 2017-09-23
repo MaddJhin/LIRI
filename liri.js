9e2927dcdb8a11c1e2f5090a15a39b9529c56afd
Francisco Carrera <franciscocarrer@gmail.com>
2017-09-22 23:33:58 -0400

    Added basic structure for handling commands. Switch statment calls different functions depending on first argument passed

--- liri.js
+++ liri.js
@@ -1,2 +1,44 @@
 var twitterKeys = require('./keys.js');
 
+// The first argument passed by the user is taken for the command
+var command = process.argv[2];
+
+// Use the command to determine which case to run
+switch (command) {
+    case 'my-tweets':
+        DisplayTweets();
+        break;
+
+    case `spotify-this-song`:
+        DisplaySong();
+        break;
+
+    case `movie-this`:
+        DisplayMovie();
+        break;
+    
+    case `do-what-it-says`:
+        FollowInstructions();
+        break;
+
+    default:
+        console.log(command + " is not a recognized option. Please use: 'my-tweets', `spotify-this-song`,  `movie-this`, or `do-what-it-says`");
+        break;
+}
+
+function DisplayTweets() {
+    console.log ("Showing last 20 tweets");
+}
+
+function DisplaySong() {
+    console.log ("Displaying song information");
+}
+
+function DisplayMovie() {
+    console.log ("Displaying movie information");
+}
+
+function FollowInstructions() {
+    console.log ("Doing what it says");
+}
+
