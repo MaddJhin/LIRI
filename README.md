f94491a6d4bb6ffec972f2ed9a084b6affc9ff33
Francisco Carrera <franciscocarrer@gmail.com>
2017-09-25 19:42:31 -0400

    Updates to swtich and parameter handling. Switch and parameter if statement moved to functions of their own. This should allow for better readability and allows -do command to function better. Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # LIRI
 Spicing up the command line
+
+node liri.js performs one of 4 functions based on the first argument given. These are -tweet, -movie, -spot, -do.
+1. -tweet
+⋅⋅⋅This returns up to the last twenty of my tweets
+
+2. -movie <Movie Name>
+⋅⋅⋅This returns basic information of a movie based on a title. All arguments after -movie are considered to be part of the movie title
+
+3. -spot <Track Name>
+⋅⋅⋅This returns basic information of a track based on a name. All arguments after -movie are considered to be part of the track search query. 
+
+4. -do
+⋅⋅⋅This executes the command on random.txt. (Nothing fancy just looks up a song).
--- liri.js
+++ liri.js
@@ -19,34 +19,43 @@ var twitter = new Twitter({
   });
 
 // The first argument passed by the user is taken for the command
-var command = process.argv[2];
-
+var params = [];
+var nodeArgs = process.argv;
 // All other arguments are used as a search string
-var searchString = "";
-if(process.argv.length > 3)
-{
-    for (let i = 3; i < process.argv.length; i++)
+
+GetParams();
+CommandSwitch(params[0], params[1]);
+
+function GetParams(){
+    var nodeArgs = process.argv;
+    
+    params.push(nodeArgs[2]);
+    // All other arguments are used as a search string 
+    if(nodeArgs.length > 3)
     {
-        searchString += process.argv[i] + " ";
+        var searchString = "";
+        for (let i = 3; i < nodeArgs.length; i++)
+        {
+            searchString += nodeArgs[i] + " ";
+        }
+        params.push(searchString);
     }
+
 }
 
-
-CommandSwitch(command);
-
 // Use the command to determine which case to run
-function CommandSwitch(action) {
+function CommandSwitch(action, params) {
     switch (action) {
         case '-tweet':
             DisplayTweets();
             break;
     
         case `-spot`:
-            DisplaySong();
+            DisplaySong(params);
             break;
     
         case `-movie`:
-            DisplayMovie();
+            DisplayMovie(params);
             break;
         
         case `-do`:
@@ -54,7 +63,7 @@ function CommandSwitch(action) {
             break;
     
         default:
-            console.log(command + " is not a recognized option. Please use: '-tweet', `-spot`,  `-movie`, or `-do`");
+            console.log(action + " is not a recognized option. Please use: '-tweet', `-spot`,  `-movie`, or `-do`");
             break;
     }   
 }
@@ -73,23 +82,22 @@ function DisplayTweets() {
     });
 }
 
-function DisplaySong() {
-    console.log ("Displaying song information");
+function DisplaySong(trackName) {
 
-    var songName = "";
-    if(process.argv.length > 3)
-        songName = encodeURI(searchString);
-    else {
-        console.log("Please put a track name");
+    if(typeof trackName == "undefined")
+    {
+        console.log("Please Enter a Track Name");
         return;
     }
-    // songName =  '"We didnt start the fire"';
 
-    spotify.search({ type: 'track', query: songName, limit: 1}, function(error, data) {
+    console.log ("Displaying track information for: " + trackName);
+    // trackName =  '"We didnt start the fire"';
+
+    spotify.search({ type: 'track', query: trackName, limit: 1}, function(error, data) {
         if (error) {
           return console.log('Error occurred: ' + error);
         }
-       
+        
         console.log("Artist: " + data.tracks.items[0].artists[0].name);
         console.log("Track Name: " + data.tracks.items[0].name);
         console.log("Preview: " + data.tracks.items[0].preview_url);
@@ -99,18 +107,18 @@ function DisplaySong() {
     });
 }
 
-function DisplayMovie() {
-    console.log ("Displaying movie information");
-    
-    var movieName = "";
-    if(process.argv.length > 3)
-        movieName = encodeURI(searchString);
-    else{
-        console.log("You didn't put a movie. Showing Mr. Nobody");
-        movieName = encodeURI('Mr. Nobody');
+function DisplayMovie(movieName) {
+
+    if(typeof movieName == "undefined")
+    {
+        console.log("Please Enter a Movie Name");
+        return;
     }
 
-    var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+    console.log ("Displaying movie information for: " + movieName);
+    
+    var movie = encodeURI(movieName);
+    var movieUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
 
     request(movieUrl, function(error, response, body) {
 
@@ -132,12 +140,16 @@ function DisplayMovie() {
 function FollowInstructions() {
     console.log ("Doing what it says");
 
-    fs.readFile("./random.txt", 'utf8', function (derp, data) {
+    fs.readFile("./random.txt", 'utf8', function (derp,  data) {
         if(derp)
             throw derp;
 
         var textCommand = data.split(',');
-        command = data[0];
-    })
+        console.log("New input arguments: ", textCommand);
+
+        var newCommand = textCommand[0];
+        var newParams = textCommand[1];
+        CommandSwitch(newCommand, newParams);
+    });
 }
 
